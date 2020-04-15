efdfad69a6fdd4cabe092da68e9e47a3448f35ed
pangadam@163.com <pangadam@163.com>
2020-04-15 10:54:11 +0800

    添加热部署插件 <dependency>             <groupId>org.springframework.boot</groupId>             <artifactId>spring-boot-devtools</artifactId>             <scope>runtime</scope>             <optional>true</optional> </dependency>

--- cloud2020-provider-user-8001/src/main/java/com/pang/cloud/controller/UserController.java
+++ cloud2020-provider-user-8001/src/main/java/com/pang/cloud/controller/UserController.java
@@ -19,13 +19,13 @@ public class UserController {
     private UserService userService;
 
     @PostMapping("/user/add")
-    public CommonResult addUser(String username) {
+    public CommonResult<Object> addUser(String username) {
         try {
             int addFlag = userService.addUser(username);
-            return new CommonResult(addFlag, "添加用户成功 - " + username, null);
+            return new CommonResult<Object>(addFlag, "添加用户成功 - " + username, null);
         } catch (Exception e) {
             log.info("添加失败", e);
-            return new CommonResult(-1, "添加用户失败", null);
+            return new CommonResult<Object>(-1, "添加用户失败", null);
         }
     }
 
