801cfd513900f1ba209314d16913b4a36212d9f2
pangadam@163.com <pangadam@163.com>
2020-04-16 12:32:31 +0800

    引入Eureka客户端

--- cloud2020-consumer-front-80/src/main/java/com/pang/cloud/ConsumerFront80Starter.java
+++ cloud2020-consumer-front-80/src/main/java/com/pang/cloud/ConsumerFront80Starter.java
@@ -2,8 +2,10 @@ package com.pang.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class ConsumerFront80Starter {
 
     public static void main(String[] args) {
